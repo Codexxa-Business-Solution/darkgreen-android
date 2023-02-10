232a6e0e0231c37b07bcb34ea3c64b5a03147b01
AshishCodexxa <ashish@codexxa.in>
2023-02-10 11:59:24 +0530

    add

---
+++ lib/constant/color.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CommonColor{
+
+  static const WHITE_COLOR = Color(0xFFFFFFFF);
+  static const BLACK_COLOR = Color(0xFF000000);
+  static const APP_BAR_COLOR = Color(0xFF008036);
+
+
+}
