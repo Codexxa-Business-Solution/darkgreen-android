8a0a805455bfa752a98d6036781234b9a9ceff9d
AshishCodexxa <ashish@codexxa.in>
2023-02-10 15:23:03 +0530

    add

--- lib/constant/color.dart
+++ lib/constant/color.dart
@@ -5,6 +5,9 @@ class CommonColor{
   static const WHITE_COLOR = Color(0xFFFFFFFF);
   static const BLACK_COLOR = Color(0xFF000000);
   static const APP_BAR_COLOR = Color(0xFF008036);
+  static const DASHBOARD_UNSELECTED_COLOR = Color(0xFF6D7188);
+  static const ICON_BACKGROUND_COLOR = Color(0xFFFFFBFB);
+  static const CIRCLE_COLOR = Color(0xFFD9D9D9);
 
 
 }
